d9ab3132c7a8bcb02db1d1afb91bd8eaf0c391c4
martalewan <marta.lewan@hotmail.com>
2026-05-06 12:15:00 +0200

    feat: add scroll to top component

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Property from "./pages/Property"
 import Properties from "./pages/Properties"
+import ScrollToTop from "./components/ScrollToTop"
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/properties" element={<Properties />} />
-      <Route path="/property/:id" element={<Property />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/properties" element={<Properties />} />
+        <Route path="/property/:id" element={<Property />} />
+      </Routes>
+    </>
+
   )
 }
